029bdf3632fb219b4f686ba38795e60081e2cd59
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-10-18 16:32:21 -0600

    remove sudo in script, script itself should be executed with sudo

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #/!bin/bash
 
 echo 'COPY scripts to ~/knex-gen/scripts'
-sudo mkdir -p ~/knex-gen/scripts
-sudo cp -r ./scripts ~/knex-gen;
-sudo chmod 711 ~/knex-gen/scripts/monoScript.sh
-sudo chmod 711 ~/knex-gen/scripts/db-setup.sh
-sudo chmod 711 ~/knex-gen/scripts/write-router.sh
-sudo chmod 711 ~/knex-gen/scripts/write-model.sh
-sudo chmod 711 ~/knex-gen/scripts/write-controller.sh
-sudo chmod 711 ~/knex-gen/scripts/test-install.sh
+mkdir -p ~/knex-gen/scripts
+cp -r ./scripts ~/knex-gen;
+chmod 711 ~/knex-gen/scripts/monoScript.sh
+chmod 711 ~/knex-gen/scripts/db-setup.sh
+chmod 711 ~/knex-gen/scripts/write-router.sh
+chmod 711 ~/knex-gen/scripts/write-model.sh
+chmod 711 ~/knex-gen/scripts/write-controller.sh
+chmod 711 ~/knex-gen/scripts/test-install.sh
 read -p 'Enter path to desired app directory: ' app
 mkdir -p $app;
 cd $app;
 echo 'Running knex-gen with ~/knex-gen/monoScript.sh'
-sudo ~/knex-gen/scripts/monoScript.sh
+~/knex-gen/scripts/monoScript.sh
